314e99452bc50b3c2561320e515b2720cda163ec
mladen <m.z.mladen@gmail.com>
2024-01-22 19:03:58 +0100

    basic student api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { StudentSchema } from './schema/student.schema';
+import { StudentService } from './student/student.service';
+import { StudentController } from './student/student.controller';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27917', {dbName: 'studentDb'})
+    MongooseModule.forRoot('mongodb://localhost:27017', {
+      dbName: 'studentDb',
+    }),
+    MongooseModule.forFeature([{ name: 'Student', schema: StudentSchema }]),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, StudentController],
+  providers: [AppService, StudentService],
 })
 export class AppModule {}
