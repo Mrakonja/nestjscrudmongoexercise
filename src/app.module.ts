dbd06a718216209cb15d7d9dcf042a404762a744
mladen <m.z.mladen@gmail.com>
2024-01-22 15:11:27 +0100

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27917', {dbName: 'studentDb'})
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
